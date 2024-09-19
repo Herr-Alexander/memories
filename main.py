d66f6c34dee5544728cf244d7e55f4f63fc483f6
Fuchs <herr.alexa.fuchs@gmail.com>
2024-09-19 18:59:57 +0200

    Triangle + sq

---
+++ main.py
@@ -0,0 +1,41 @@
+import turtle
+
+"""Квадрати"""
+
+# def square(lenth):
+#     for i in range(4):
+#         abc.color(colors[i % 4])
+#         abc.forward(lenth)
+#         abc.left(90)
+#
+# colors = ["red", "blue", "brown", "green"]
+# abc = turtle.Turtle()
+# abc.speed(5)
+#
+# dovgyna = 40
+# for i in range(36):
+#     abc.circle(dovgyna)
+#     abc.right(10)
+#     dovgyna += 5
+
+"""Трикутники та квадрати"""
+
+pen = turtle.Turtle()
+
+pen.speed(1)
+
+# Квадрат
+n = 5
+sum_angle = (n - 2) * 180
+if sum_angle % n == 0:
+    angle = sum_angle // n
+    for i in range(n):
+        pen.forward(100)
+        pen.left(180 - angle)
+
+# Трикутник
+angle = 60
+n = 3
+for i in range(n):
+    pen.forward(100)
+    pen.left(180 - angle)
