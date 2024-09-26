4077ffc3db38c8f826f4c0c31b225a76480acb14
Александр <herr.alexa.fuchs@gmail.com>
2024-09-26 19:02:56 +0200

    26.09.2024 - v2

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import turtle
+import turtle, time
 
 """Квадрати"""
 
@@ -42,16 +42,62 @@ import turtle
 
 """3-10 кутники"""
 
+# bob = turtle.Turtle()
+# bob.speed(1)
+# bob.up()
+# bob.setposition(-50, -50)
+# bob.down()
+#
+# def abc(n, dovgyna):
+#     sum_angle = (n - 2) * 180
+#     if sum_angle % n == 0:
+#         angle = sum_angle // n
+#         for i in range(n):
+#             bob.forward(100)
+#             bob.left(180 - angle)
+#
+# for i in range(3, 11):
+#     abc(i, 50)
+
+"""Зiрка"""
+
+# bob = turtle.Turtle()
+# bob.speed(5)
+#
+# def star_fill(n, dovgyna):
+#     bob.begin_fill()
+#     star(n, dovgyna)
+#     bob.end_fill()
+#
+# def star(n, dovgyna):
+#     if n % 2 != 0:
+#         for i in range(n):
+#             bob.forward(dovgyna)
+#             angle = n // 2 * 360 / n
+#             bob.left(angle)
+#
+# for i in range(5, 16, 2):
+#     star_fill(i, 150)
+#     time.sleep(1)
+#     bob.reset()
+
+"""Зоряне небо"""
+
 bob = turtle.Turtle()
+bob.color("yellow")
 bob.speed(1)
 
-def abc(n, dovgyna):
-    sum_angle = (n - 2) * 180
-    if sum_angle % n == 0:
-        angle = sum_angle // n
+def star_fill(n, dovgyna):
+    bob.begin_fill()
+    if n % 2 != 0:
         for i in range(n):
-            bob.forward(100)
-            bob.left(180 - angle)
+            bob.forward(dovgyna)
+            angle = n // 2 * 360 / n
+            bob.left(angle)
+    bob.end_fill()
 
-for i in range(3, 11):
-    abc(i, 50)
+star_fill(5, 150)
+
+window = turtle.Screen()
+window.bgcolor("black")
+time.sleep(5)
